d4bca17e14641f36bf96e4ca7db08716cede8e92
Dmytryi Striletskyi <dmytryi.striletskyi@gmail.com>
2017-03-30 23:24:21 +0300

    Modify urls

--- rutetider (django, api)/urls.py
+++ rutetider (django, api)/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='https://github.com/DmytryiStriletskyi/Rutetider/wiki')),
 
     # Rutetider
-    url(r'^timetable/$', views.TimetableApplication.as_view()),
+    url(r'^timetable', views.TimetableApplication.as_view()),
     url(r'^timetable/clear_timetable', views.ClearTimetable.as_view()),
     url(r'^timetable/add_lesson', views.AddLesson.as_view()),
     url(r'^timetable/get_lesson', views.GetLessons.as_view()),
@@ -16,21 +16,21 @@ urlpatterns = [
     url(r'^timetable/get_all_groups', views.GetAllGroups.as_view()),
 
     # Subscribers
-    url(r'^statistic/$', views.SubscribersApplication.as_view()),
-    url(r'^statistic/clear_subscribers', views.ClearSubscribers.as_view()),
-    url(r'^statistic/add_subscriber', views.AddSubscriber.as_view()),
-    url(r'^statistic/get_subscriber_group', views.GetSubscriberGroup.as_view()),
-    url(r'^statistic/is_subscriber', views.IsSubscriber.as_view()),
-    url(r'^statistic/unsubscribe', views.UnSubscribe.as_view()),
+    url(r'^subscribers', views.SubscribersApplication.as_view()),
+    url(r'^subscribers/clear_subscribers', views.ClearSubscribers.as_view()),
+    url(r'^subscribers/add_subscriber', views.AddSubscriber.as_view()),
+    url(r'^subscribers/get_subscriber_group', views.GetSubscriberGroup.as_view()),
+    url(r'^subscribers/is_subscriber', views.IsSubscriber.as_view()),
+    url(r'^subscribers/unsubscribe', views.UnSubscribe.as_view()),
 
     # CurrentDates
-    url(r'^currentdates/$', views.CurrentDatesApplication.as_view()),
+    url(r'^currentdates', views.CurrentDatesApplication.as_view()),
     url(r'^currentdates/clear_current_dates', views.ClearCurrentDates.as_view()),
     url(r'^currentdates/add_current_dates', views.AddDates.as_view()),
     url(r'^currentdates/get_current_dates', views.GetDates.as_view()),
 
     # UserPosition
-    url(r'^userposition/$', views.UserPositionApplication.as_view()),
+    url(r'^userposition', views.UserPositionApplication.as_view()),
     url(r'^userposition/clear_user_position', views.ClearUserPosition.as_view()),
     url(r'^userposition/clear_user_data', views.ClearUserData.as_view()),
     url(r'^userposition/set_getting_position', views.SetGettingPosition.as_view()),
@@ -46,7 +46,7 @@ urlpatterns = [
     url(r'^userposition/back_keyboard', views.BackKeyboard.as_view()),
 
     # Statistic
-    url(r'^statistics/$', views.StatisticsApplication.as_view()),
+    url(r'^statistics', views.StatisticsApplication.as_view()),
     url(r'^statistics/clear_statistics', views.ClearStatistics.as_view()),
     url(r'^statistics/add_statistics', views.AddStatistics.as_view()),
     url(r'^statistics/get_statistics_general', views.GetStatisticsGeneral.as_view()),
